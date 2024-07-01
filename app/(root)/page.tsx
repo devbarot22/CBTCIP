587863b8823947358a28efa510ace2d3fd42d9c0
devbarot22 <dbarot717@gmail.com>
2024-07-01 13:40:44 +0530

    changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "../../components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
